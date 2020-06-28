899a3e5b2d400f51989e1dce2fb6ed3b343e4825
Tiago-ts <tiagogatewayts@gmail.com>
2020-06-28 09:16:25 -0300

    api

---
+++ Ecoleta/script/create-point.js
@@ -0,0 +1,50 @@
+function popularUfs() {
+    const ufSelect = document.querySelector("select[name=uf]")
+    fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
+    //.then( (res) => {return res.json()})
+    .then ( res => res.jason())
+    .then( states => {
+        for( const state of states ){
+
+             ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
+        }
+       
+    })
+}
+
+popularUfs()
+
+function getCities(event){
+    const citySelect = document.querySelector("select[name=city]")
+
+    const stateInput = document.querySelector("name=state")
+
+    console.log(event.target.value)
+
+    const ufValue = event.target.value
+
+    const indexOfSelectedState = event.target.selectedIndex
+
+    stateInput.value = event.target.options[indexOfSelectedState].text
+
+    const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
+
+    fetch(url)
+
+    .then ( res => res.jason())
+    .then( cities => {
+        for( const city of cities ){
+
+             ufSelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+        }
+
+        citySelect.disabled = false
+       
+    })
+
+
+}
+
+document
+   .querySelector("select[name=uf]")
+   .addEventListener("change", getCities)
